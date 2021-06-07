946553b3063e071ad32dab45afe9218eeaa8b9ae
boqiu <82121246@qq.com>
2021-06-07 17:45:24 +0800

    Add utilities for internal contracts

---
+++ contracts/InternalContracts/InternalContractsHandler.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../utils/ERC1820Context.sol";
+import "./SponsorWhitelistControl.sol";
+import "./AdminControl.sol";
+
+/**
+ * @dev Provides default configurations for Conflux internal contracts.
+ */
+abstract contract InternalContractsHandler is ERC1820Context {
+
+    constructor() {
+        if (!_cfxChain) {
+            return;
+        }
+
+        // Support to sponsor all users by default.
+        address[] memory users = new address[](1);
+        users[0] = address(0);
+        SponsorWhitelistControl(0x0888000000000000000000000000000000000001).addPrivilege(users);
+
+         // remove contract admin
+         AdminControl ac = AdminControl(0x0888000000000000000000000000000000000000);
+        ac.setAdmin(address(this), address(0));
+        require(
+            ac.getAdmin(address(this)) == address(0),
+            "require admin == null"
+        );
+    }
+
+}
---
+++ contracts/utils/ERC1820Context.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/introspection/IERC1820Registry.sol";
+
+/**
+ * @dev Supports ERC1820 registry on both ETH and Conflux.
+ */
+abstract contract ERC1820Context {
+    using Address for address;
+
+    address constant private ERC1820_REGISTRY_ETH = 0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24;
+    address constant private ERC1820_REGISTRY_CFX = 0x88887eD889e776bCBe2f0f9932EcFaBcDfCd1820;
+
+    bool _cfxChain;
+
+    constructor() {
+        _cfxChain = !ERC1820_REGISTRY_ETH.isContract();
+    }
+
+    function _erc1820Registry() internal view returns (IERC1820Registry) {
+        if (_cfxChain) {
+            return IERC1820Registry(ERC1820_REGISTRY_CFX);
+        } else {
+            return IERC1820Registry(ERC1820_REGISTRY_ETH);
+        }
+    }
+}
