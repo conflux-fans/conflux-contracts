d1ea4ded978342386d1ca9c020e9ca9438250cc7
W <pana.wang@outlook.com>
2021-10-24 17:10:30 +0800

    add new internal contracts (#19)
    * add new internal contracts
    
    Co-authored-by: PanaW <pan.wang@confluxnetwork.org>

--- contracts/InternalContracts/InternalContractsLib.sol
+++ contracts/InternalContracts/InternalContractsLib.sol
@@ -5,6 +5,9 @@ pragma solidity ^0.8.0;
 import "./AdminControl.sol";
 import "./SponsorWhitelistControl.sol";
 import "./Staking.sol";
+import "./EpochNumberContext.sol";
+import "./PoSRegister.sol";
+import "./ReentrancyConfig.sol";
 
 /**
  * @dev This library includes initialized Internal Contract instance.
@@ -25,4 +28,19 @@ library InternalContracts {
      * @dev staking
      */
     Staking public constant STAKING = Staking(0x0888000000000000000000000000000000000002);
+
+    /**
+     * @dev EpochNumberContext
+     */
+    Context public constant EPOCH_CONTEXT = Context(0x0888000000000000000000000000000000000003);
+
+    /**
+     * @dev Reentrancy
+     */
+    ReentrancyConfig public constant REENTRANCY = ReentrancyConfig(0x0888000000000000000000000000000000000004);
+
+    /**
+     * @dev PoSRegister
+     */
+    PoSRegister public constant POS_REGISTER = PoSRegister(0x0888000000000000000000000000000000000005);
 }
