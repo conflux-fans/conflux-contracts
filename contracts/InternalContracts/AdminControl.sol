22b4cb50400ed32979db06756b5432d651fc15bc
W <pana.wang@outlook.com>
2021-11-02 16:52:23 +0800

    Change internal contracts to interface (#22)
    * update internal contracts to interface
    
    Co-authored-by: PanaW <pan.wang@confluxnetwork.org>

--- contracts/InternalContracts/AdminControl.sol
+++ contracts/InternalContracts/AdminControl.sol
@@ -1,24 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.15;
 
-contract AdminControl {
+interface AdminControl {
     /*** Query Functions ***/
     /**
      * @dev get admin of specific contract
      * @param contractAddr The address of specific contract
      */
-    function getAdmin(address contractAddr) public view returns (address) {}
+    function getAdmin(address contractAddr) external view returns (address);
 
     /**
      * @dev Contract admin set the administrator of contract `contractAddr` to `newAdmin`.
      * @param contractAddr The address of the contract
      * @param newAdmin The new admin address
      */
-    function setAdmin(address contractAddr, address newAdmin) public {}
+    function setAdmin(address contractAddr, address newAdmin) external;
 
     /**
      * @dev Contract admin destroy contract `contractAddr`.
      * @param contractAddr The contract to be destroied
      */
-    function destroy(address contractAddr) public {}
+    function destroy(address contractAddr) external;
 }
